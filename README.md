7d2a43b311bcc5885dcf5c2079e4e422beab1d3a
Derek Miranda <derek.miranda@ff0000.com>
2019-03-28 16:22:33 -0700

    Pkg: rename to black-iris
    - Redist

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Roma Animation Preset
+# Black Iris Animation Preset
 
 Black iris wipe into keyart scaling in and other endframe elements fading in
--- dist/npm/e36bb5b4-a228-4ad2-8f04-9781fde295d5.js
+++ dist/npm/e36bb5b4-a228-4ad2-8f04-9781fde295d5.js
@@ -5,14 +5,14 @@ if (typeof module === 'undefined') {
 // prettier-ignore
 module.exports = {
 	"id": "e36bb5b4-a228-4ad2-8f04-9781fde295d5",
-	"name": "an-ER-roma",
+	"name": "an-ER-black-iris",
 	"description": "Black iris wipe into keyart scaling in and other endframe elements fading in",
 	"type": "animations",
 	"context": "Default",
 	"state": "published",
 	"updated": 1553801568961,
-	"full_name": "NetflixDev/an-ER-roma",
-	"html_url": "https://github.com/NetflixDev/an-ER-roma",
+	"full_name": "NetflixDev/an-ER-black-iris",
+	"html_url": "https://github.com/NetflixDev/an-ER-black-iris",
 	"username": "GitHub",
 	"version": "5.0.0",
 	"minimum": "3.0.0"
